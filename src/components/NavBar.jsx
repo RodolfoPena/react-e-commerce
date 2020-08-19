f5d57f99a0bf60946c7ed15ee355e504fc741d32
RodolfoPena <rodolfo.p.ing@gmail.com>
2020-08-18 21:10:17 -0400

    primer componente navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => (
+
+<div>
+    <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Canasta</li>
+    </ul>
+
+</div>
+
+)
+
+export default NavBar;
